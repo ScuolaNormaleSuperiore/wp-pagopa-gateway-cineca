7aeb2273446bf38e22340211d8683baceba86f5e
Claudio Battaglino <claudio.battaglino@sns.it>
2021-12-09 14:42:28 +0100

    *Encrypt the token before using it

---
+++ inc/encryption-manager.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright: © 2021-2022, SNS
+ * License: GNU General Public License v3.0
+ *
+ * @author      ICT Scuola Normale Superiore
+ * @category    Payment Module
+ * @package     PagoPA Gateway Cineca
+ * @version     1.0.3-b1
+ * @copyright   Copyright (c) 2021 SNS)
+ * @license     GNU General Public License v3.0
+ */
+
+DEFINE( 'CIPHER', 'aes-256-ctr' );
+DEFINE( 'IV_KEY', 'p2345hd9101f45t1' );
+
+/**
+ * EncriptionManager class
+ */
+class Encryption_Manager {
+
+	/**
+	 * Encrypt the text passed as parameter.
+	 *
+	 * @param String $text - The text to be encrypted.
+	 * @param String $key - The key for the encription.
+	 * @return string
+	 */
+	public static function encrypt_text( $text, $key ) {
+		return openssl_encrypt( $text, CIPHER, $key, 0, IV_KEY );
+	}
+
+	/**
+	 * Decrypt the text passed as parameter.
+	 *
+	 * @param String $text - The text to be decrypted.
+	 * @param String $key - The key for the decryption.
+	 * @return string
+	 */
+	public static function decrypt_text( $text, $key ) {
+		return openssl_decrypt( $text, CIPHER, $key, 0, IV_KEY );
+	}
+
+}
