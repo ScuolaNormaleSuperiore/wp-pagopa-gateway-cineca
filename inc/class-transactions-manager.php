e9f7fa9a9e39f2f715a071cce6a6cb43d8667fcb
Claudio Battaglino <claudio.battaglino@sns.it>
2022-01-10 15:51:16 +0100

    Created new version

--- inc/class-transactions-manager.php
+++ inc/class-transactions-manager.php
@@ -6,7 +6,7 @@
  * @author      ICT Scuola Normale Superiore
  * @category    Payment Module
  * @package     PagoPA Gateway Cineca
- * @version     1.0.10-b1
+ * @version     1.0.11-beta
  * @copyright   Copyright (c) 2021 SNS)
  * @license     GNU General Public License v3.0
  */
