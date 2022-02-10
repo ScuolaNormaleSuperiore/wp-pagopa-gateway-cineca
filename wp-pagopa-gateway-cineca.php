66e6abcb2d91548ce4c57167c80003f9cc065188
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2022-02-10 11:21:26 +0100

    Added basic-authentication for the local API

--- wp-pagopa-gateway-cineca.php
+++ wp-pagopa-gateway-cineca.php
@@ -166,11 +166,11 @@ function wp_gateway_pagopa_init() {
 			$this->enabled     = $this->get_option( 'enabled' );
 			$this->testmode    = $this->get_option( 'testmode' );
 			$this->api_user    = ( 'yes' === $this->testmode ) ?
-				$this->get_option( 'api_user_test' ) :
-				$this->get_option( 'api_user_prod') ;
+				trim( $this->get_option( 'api_user_test' ) ) :
+				trim( $this->get_option( 'api_user_prod') );
 			$this->api_pwd     = ( 'yes' === $this->testmode ) ?
-				$this->get_option( 'api_pwd_test' ) :
-				$this->get_option( 'api_pwd_prod') ;
+				trim( $this->get_option( 'api_pwd_test' ) ) :
+				trim( $this->get_option( 'api_pwd_prod') );
 
 			// This action hook saves the settings.
 			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
@@ -700,8 +700,8 @@ function wp_gateway_pagopa_init() {
 		 * @return void
 		 */
 		public function webhook_transaction_notification( $args ) {
-			$this->verifyAPIAuthentication();
 			$this->log_action( 'info', 'webhook_transaction_notification' );
+			$this->verifyAPIAuthentication();
 			$result    = trim( file_get_contents( 'php://input' ) );
 			// SimpleXML seems to have problems with the colon ":" in the <xxx:yyy> response tags, so take them out.
 			$xml      = preg_replace( '/(<\/?)(\w+):([^>]*>)/', '$1$2$3', $result );
@@ -767,11 +767,12 @@ function wp_gateway_pagopa_init() {
 		 * @return boolen - True if the account is right.
 		 */
 		private function verifyAPIAuthentication() {
-			$api_username  = $this->api_user ?  $this->api_user : '';
-			$api_password  = $this->api_pwd ?  $this->api_pwd : '';
+			$this->log_action( 'info', json_encode( apache_request_headers() ) );
+			$api_username  = $this->api_user;
+			$api_password  = $this->api_pwd;
 			if ( $api_username && $api_username ) {
 				$auth          = apache_request_headers();
-				$authorization = $auth['Authorization'];
+				$authorization = isset( $auth['Authorization'] ) ? $auth['Authorization'] : '';
 				$valid_token   = 'Basic ' . base64_encode( $api_username . ':' . $api_password );
 				if ( $authorization !== $valid_token ){
 					header( 'HTTP/1.1 401 Unauthorized' );
@@ -780,7 +781,6 @@ function wp_gateway_pagopa_init() {
 			}
 			return true;
 		}
-
 
 		/**
 		 * Hook called to start scheduled actions.
