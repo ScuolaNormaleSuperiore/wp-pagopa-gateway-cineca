4189d2e6385e860eab7efaebbdc94632a7830c68
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2024-01-05 12:12:20 +0100

    Nuova versione

--- inc/class-log-list-table.php
+++ inc/class-log-list-table.php
@@ -6,7 +6,7 @@
  * @author      ICT Scuola Normale Superiore
  * @category    Payment Module
  * @package     PagoPA Gateway Cineca
- * @version     1.1.6
+ * @version     1.1.7
  * @copyright   Copyright (c) 2021 SNS)
  * @license     GNU General Public License v3.0
  */
