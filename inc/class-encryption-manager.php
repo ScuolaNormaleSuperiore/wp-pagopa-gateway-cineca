e55e2a7dd4a5a902b762d9799eba5d4ad9488cd2
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2024-01-15 15:36:35 +0100

    Nuova versione

--- inc/class-encryption-manager.php
+++ inc/class-encryption-manager.php
@@ -6,7 +6,7 @@
  * @author      ICT Scuola Normale Superiore
  * @category    Payment Module
  * @package     PagoPA Gateway Cineca
- * @version     1.1.7
+ * @version     1.1.8
  * @copyright   Copyright (c) 2021 SNS)
  * @license     GNU General Public License v3.0
  */
