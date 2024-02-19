acbf8a4800d72166334843102f3be9d45de75c23
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2024-02-19 11:57:57 +0100

    Refactoring: utilizzo di IIFE per incapsulare il codice e impedire che le variabili definite al suo interno abbiano uno scope globale

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,17 +1,20 @@
-const wpgw_settings = window.wc.wcSettings.getSetting( 'wp_gateway_pagopa_data', {} );
-const wpgw_label = window.wp.htmlEntities.decodeEntities( wpgw_settings.title ) || window.wp.i18n.__( 'WP Gateway PagoPa', 'pagopa_gateway_cineca' );
-const WPGW_Content = () => {
-    return window.wp.htmlEntities.decodeEntities( wpgw_settings.description || '' );
-};
-const WPGW_Block_Gateway = {
-    name: 'pagopa_gateway_cineca',
-    label: wpgw_label,
-    content: Object( window.wp.element.createElement )( WPGW_Content, null ),
-    edit: Object( window.wp.element.createElement )( WPGW_Content, null ),
-    canMakePayment: () => true,
-    ariaLabel: wpgw_label,
-    supports: {
-        features: wpgw_settings.supports,
-    },
-};
-window.wc.wcBlocksRegistry.registerPaymentMethod( WPGW_Block_Gateway );
+(function() => {
+	"use strict";
+	const settings = window.wc.wcSettings.getSetting( 'wp_gateway_pagopa_data', {} );
+	const label = window.wp.htmlEntities.decodeEntities( settings.title ) || window.wp.i18n.__( 'WP Gateway PagoPa', 'pagopa_gateway_cineca' );
+	const Content = () => {
+			return window.wp.htmlEntities.decodeEntities( settings.description || '' );
+	};
+	const Block_Gateway = {
+			name: 'pagopa_gateway_cineca',
+			label: label,
+			content: Object( window.wp.element.createElement )( Content, null ),
+			edit: Object( window.wp.element.createElement )( Content, null ),
+			canMakePayment: () => true,
+			ariaLabel: label,
+			supports: {
+					features: settings.supports,
+			},
+	};
+	window.wc.wcBlocksRegistry.registerPaymentMethod( Block_Gateway );
+})();
